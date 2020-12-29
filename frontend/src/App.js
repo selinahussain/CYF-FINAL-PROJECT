d137c408f45dd4aa6b9539ad1b52ec1e7a9ae62e
Osman <osmanhajr32@gmail.com>
2020-12-29 11:14:30 +0000

    update the frontend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import './App.css';
 import Header from './Components/HeaderComponent/HeaderComponent.';
+import Routes from './Route';
 // import { Button } from 'react-bootstrap';
 // import { Form } from 'react-bootstrap';
 function App() {
   return (
     <div className="">
       <Header />
+      <Routes />
     </div>
   );
 }
