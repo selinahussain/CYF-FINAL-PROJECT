8d71f52b9cbf8d74998ff1f38370baa13e9f6d7c
Selina Hussain <61423748+selinahussain@users.noreply.github.com>
2021-01-06 21:12:28 +0000

    student sidebar styling

--- frontend/src/components/StudentComponents/StudentSideBar.jsx
+++ frontend/src/components/StudentComponents/StudentSideBar.jsx
@@ -2,30 +2,38 @@ import React from 'react';
 import Logo from "../../Assets/icons/user.png";
 import StudentImage  from "../../Assets/images/student_page_logo.png";
 import "./StudentSideBar.scss";
+import { useAuth } from "../../Auth/use-auth";
 
 export default function StudentSideBar() {
+    let auth = useAuth();
     return (
-        <div className = "container">
+        <div className = "container" >
+        <div className= "wrapper">
             <div className = "user_icon_container">
-                <div className="user_icon_image">
+                <div className="user_icon_image" >
                 <img 
-                
                 alt = "user icon" 
                 src ={Logo}/>
                 </div>
-                <p>Selina Hussain</p>
-                <p>Student</p>
-            </div><hr/>
+                <h3>{auth.user.name}</h3>
+                <h5><small>Student</small></h5>
+            </div>
+
+            <hr/>
 
             <div>
-                <h3>Student Confident Tracker</h3>
-            </div><hr/>
-            <div>
+                <h3>Student Confidence Tracker</h3>
+            </div>
+
+            <hr/>
+            <div className="student_icon_container">
+                <div className = "student_icon_image">
                 <img 
-                className = ""
                 alt = ""
                 src = {StudentImage}
                 />
+                </div>
+            </div>
             </div>
         </div>
     )
