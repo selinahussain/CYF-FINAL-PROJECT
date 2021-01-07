03f53ae25ef9aaef2e48331334eaeb845fa1cb31
Osman <osmanhajr32@gmail.com>
2021-01-07 15:11:25 +0000

    my changes

--- frontend/src/Pages/StudentPages/StudentMainPage.js
+++ frontend/src/Pages/StudentPages/StudentMainPage.js
@@ -2,7 +2,7 @@
 import './StudentMainPage.scss';
 import StudentSideBar from '../../components/StudentComponents/StudentSideBar';
 import StudentModulesBody from '../../components/StudentComponents/StudentModulesBody';
-import JavaScript from '../../components/StudentComponents/Modules Components/JavaScript/JavaScript';
+
 
 
 const StudentMainPage = () => {
@@ -12,7 +12,7 @@ const StudentMainPage = () => {
      <h1>Hi I am Student main page</h1>
      <StudentModulesBody />
      <StudentSideBar />
-     <JavaScript/>
+     
     </div>
   );
 };
