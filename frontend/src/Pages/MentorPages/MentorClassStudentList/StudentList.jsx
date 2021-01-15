cd289d85f7a9a21e68cfa558ee1dade7901e3ad7
Selina Hussain <61423748+selinahussain@users.noreply.github.com>
2021-01-15 10:34:46 +0000

    class/student list

--- frontend/src/Pages/MentorPages/MentorClassStudentList/StudentList.jsx
+++ frontend/src/Pages/MentorPages/MentorClassStudentList/StudentList.jsx
@@ -1,56 +1,99 @@
-import React, {useState} from 'react'
-import useFetch from '../../../Auth/useFetch';
-import Spinner from '../../../components/UI/Spinner'
+import React, { useState } from "react";
+import useFetch from "../../../Auth/useFetch";
+import Spinner from "../../../components/UI/Spinner";
 import { Route, Link } from "react-router-dom";
-import './StudentList.scss'
+import "./StudentList.scss";
 import "antd/dist/antd.css";
-import MentorIndividualGradeList from '../MentorIndividualGradeList/MentorIndividualGradeList';
-
-
-
+import MentorIndividualGradeList from "../MentorIndividualGradeList/MentorIndividualGradeList";
+import MentorSideBar from "../../../components/MentorComponents/MentorSideBar";
+import { Button } from "antd";
+import "antd/dist/antd.css";
 
 export default function StudentList() {
-  let { status, data, error } = useFetch('http://localhost:3001/api/region/classes/studentList');
-  if (status === 'error') {
+  let { status, data, error } = useFetch(
+    "http://localhost:3001/api/region/classes/studentList"
+  );
+  if (status === "error") {
     return <div>Error: {error.message}</div>;
-  } else if (status === 'success') {
-    return <div> <AllStudents data={data} />  </div>;
+  } else if (status === "success") {
+    return (
+      <div>
+        {" "}
+        <AllStudents data={data} />{" "}
+      </div>
+    );
   } else {
     return <Spinner />;
   }
 }
 
+const styling = {
+  stroke: "rgb(209, 46, 47)",
+  "stroke-width": 9,
+};
 
-
-
-
-
- 
-
-
-const AllStudents = ({data}) =>{
-  const [students, setStudents] = useState(data)
-  return(
-    <div className = "container">
-     <div className="row">
-        {students.map((student) => (
-          <div className= "moduleDiv">
-            <div className="text-center modules" key={student.id}>
-              <Link to={`/students/${student.id}`}>
-
-                <h2 className="">{student.name}</h2>
-                <h2>{student.id}</h2>
-              </Link>
-
-
-            </div>
-            <div>
-            <Route path={`/students/${student.id}`} component={MentorIndividualGradeList} />
-          </div>
-          </div>
-        ))}
+const AllStudents = ({ data }) => {
+  const [students, setStudents] = useState(data);
+  return (
+    <div className="row">
+      <div className="col-sm-6 col-md-3 col-lg-3">
+        <MentorSideBar />
       </div>
-   </div>
-  )
- 
-}
+      <div className="col-sm-6 col-md-9 col-lg-9">
+        <div className="row">
+          <div className="col-1">
+            <Button
+              href="/regions/classes/West%20Midlands"
+              className="backLink btn-lg rounded-lg"
+            >
+              Back To Classes
+            </Button>
+          </div>
+        </div>
+        <div className="row">
+          <h1>Student List</h1>
+
+          <svg height="30" width="700">
+            <line x1="0" y1="0" x2="100%" y2="0" style={styling} />
+          </svg>
+        </div>
+        <div className="row">
+          <div className="col-12 text-center">
+            <h4> How to get started:</h4>
+            <p>
+              Below are links to students from your class!
+              <br />
+              You will be redirected to a separate page, depending on the
+              student you click on, where you will be able to see a general
+              average of each module out of 100%. This is what students have
+              rated for themselves and how they believe they are doing for the
+              module.
+              <br /> It is a perfect place for you to be able to keep track of
+              how students are doing on the course.
+            </p>
+          </div>
+          <svg height="40" width="100%">
+            <line x1="0" y1="0" x2="100%" y2="0" style={styling} />
+          </svg>
+        </div>
+        <div className="row">
+          {students.map((student) => (
+            <div className="moduleDiv col-6">
+              <div className="text-center modules" key={student.id}>
+                <Link to={`/students/${student.id}`}>
+                  <h2 className="">{student.name}</h2>
+                </Link>
+              </div>
+              <div>
+                <Route
+                  path={`/students/${student.id}`}
+                  component={MentorIndividualGradeList}
+                />
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
--- frontend/src/components/MentorComponents/Classes/WestMidlands Classes/WestMidlandClasses.jsx
+++ frontend/src/components/MentorComponents/Classes/WestMidlands Classes/WestMidlandClasses.jsx
@@ -1,54 +1,83 @@
-
-
-
-
-
-
-import { useState } from 'react';
-import { Link,Route} from 'react-router-dom';
-import useFetch from '../../../../Auth/useFetch';
-import StudentList from '../../../../Pages/MentorPages/MentorClassStudentList/StudentList';
-import Spinner from '../../../UI/Spinner';
-import ComingSoon from '../../Regions/ComingSoon';
-//import './MentorRegions.scss';
+import { useState } from "react";
+import { Link, Route } from "react-router-dom";
+import useFetch from "../../../../Auth/useFetch";
+import StudentList from "../../../../Pages/MentorPages/MentorClassStudentList/StudentList";
+import Spinner from "../../../UI/Spinner";
+import ComingSoon from "../../Regions/ComingSoon";
+import { Button } from "antd";
+import "antd/dist/antd.css";
+import ClassImage from "../../../../Assets/images/class.png";
+import inProgress from "../../../../Assets/images/inprogress.png";
+import "./WestMidlandClasses.scss";
 
 const WestMidlandClasses = () => {
-  let { status, data, error } = useFetch('http://localhost:3001/api/region/classes');
- console.log(data);
-  if (status === 'error') {
+  let { status, data, error } = useFetch(
+    "http://localhost:3001/api/region/classes"
+  );
+  console.log(data);
+  if (status === "error") {
     return <div>Error: {error.message}</div>;
-  } else if (status === 'success') {
+  } else if (status === "success") {
     return <ClassList data={data} />;
   } else {
     return <Spinner />;
   }
 };
 
+const styling = {
+  stroke: "rgb(209, 46, 47)",
+  "stroke-width": 9,
+};
+
 const ClassList = ({ data }) => {
   const [regions, setRegions] = useState(data);
 
-
   return (
     <div>
-        <h2> Please Select a Classes</h2>
+      <div className="backDiv">
+        <Button href="/region" className="backLink btn-lg rounded-lg">
+          Back To Regions
+        </Button>
+      </div>
+
+      <h1> Please Select a Class:</h1>
+      <svg height="150" width="100%">
+        <line x1="0" y1="0" x2="100%" y2="0" style={styling} />
+      </svg>
+
       <div className="row">
-         { regions.map(({ name }) => (
-            <div className="col-12 col-md-6 col-xl-4 rounded " key={name}>
-              <div className="text-center image-container">
-                <Link to={`/West Midlands/${name}/Students`}>
-  
-                  <h2 className="region">{name}</h2>
-                </Link>
-              </div>
-             
+        {regions.map(({ name }) => (
+          <div className="col-12 col-md-6  rounded " key={name}>
+            <div className="text-center image-container">
+              <Link to={`/West Midlands/${name}/Students`}>
+                <img
+                  src={
+                    name === "West Midlands Class 1"
+                      ? ClassImage
+                      : name === "West Midlands Class 2"
+                      ? ClassImage
+                      : inProgress
+                  }
+                  className="rounded mx-auto classesImages"
+                  alt={name}
+                />
+
+                <h2 className="region">{name}</h2>
+              </Link>
             </div>
-          ))
-        }
+          </div>
+        ))}
       </div>
       <div>
-            <Route path={`/West Midlands/West Midlands Class 1/Students`} component={StudentList} />
-            <Route path={`/West Midlands/West Midlands Class 2/Students`} component={ComingSoon} />
-          </div>
+        <Route
+          path={`/West Midlands/West Midlands Class 1/Students`}
+          component={StudentList}
+        />
+        <Route
+          path={`/West Midlands/West Midlands Class 2/Students`}
+          component={ComingSoon}
+        />
+      </div>
     </div>
   );
 };
